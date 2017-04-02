a341a67818c387351a787d7dcd14eb5468e6f646
MrFricker <switzerland@outlook.co.uk>
2017-04-02 13:41:36 +0100

    PHPDoc comments

--- src/GMP.php
+++ src/GMP.php
@@ -3,22 +3,42 @@
 namespace DavidFricker\BaseChanger;
 
 /**
-  * Enables alphanumeric encoding and decoding of binary data for easy transportation over a medium
-  * Works using the GMP extension to avoid PHP's base_convert loss of preceision when working with large numbers e.g. a 256 bit key
+  * GMP Base converter
+  * 
+  * Enables alphanumeric encoding and decoding of binary data for easy 
+  * transportation over a medium. Works using the GMP extension to avoid 
+  * PHP's base_convert loss of preceision when working with large numbers.
   *
-  * Class is optimized for conversion of input to/from binary to aritbrary bases
-  * however it also provides an artibrtary base changer in baseChange()
+  * Class is optimized for conversion of input to/from binary to aritbrary bases.
+  * However it also provides an artibrtary base changer in baseChange().
 */
 class GMP {
+    /**
+     * Intermediate base between binary and the base of choice.
+     */
    const INTERMEDIATE_BASE = 16;
 
+   /**
+    * Convert raw binary bytes to arbitrary base
+    * 
+    * @param  binary $binary_data binary bytes not binary string (e.g. 1011)
+    * @param  integer $base_to base to convert to
+    * @return string encoded binary data
+    */
    public static function changeTo($binary_data, $base_to) {
       $hexadecimal = bin2hex($binary_data);
 
       return self::baseChange($hexadecimal, self::INTERMEDIATE_BASE, $base_to);
    }
 
-   public static function changeFrom($encoded_hex, $base_from) {
+   /**
+    * Convert from arbitrary base to raw binary data
+    * 
+    * @param  string $encoded representation of binary data 
+    * @param  integer $base_from current base of $encoded
+    * @return binary raw binary data
+    */
+   public static function changeFrom($encoded, $base_from) {
       $hexadecimal = self::baseChange($encoded_hex, $base_from, self::INTERMEDIATE_BASE); 
 
       // if its not even it will be rejected by hex2bin
@@ -31,12 +51,13 @@ class GMP {
    }
 
    /**
-    * This method cannot convert raw binary bytes. Ethier encode the binary in string format (e.g. 1011) or change it to hex and then change to your desired base
+    * This method cannot convert raw binary bytes. 
+    * Either encode the binary in string format (e.g. 1011) or change it to hex and then change to your desired base.
     * 
-    * @param  [type] $input     [description]
-    * @param  [type] $base_from [description]
-    * @param  [type] $base_to   [description]
-    * @return [type]            [description]
+    * @param  mixed $input raw binary or string encoded version binary
+    * @param  integer $base_from current base of $input value
+    * @param  integer $base_to base to convert to
+    * @return string representation of $input in arbitrary base
     */
    public static function baseChange($input, $base_from, $base_to) {
       return gmp_strval(gmp_init($input, $base_from), $base_to);
